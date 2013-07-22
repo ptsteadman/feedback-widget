b2a0f0daebc385c1862102babcaf766cd1db22bb
Patrick <uhhonestly@gmail.com>
2013-07-22 15:10:39 -0400

    Trying hook.

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ config.mysql.host = process.env.MYSQL_HOST ||'host';
 config.mysql.user = process.env.MYSQL_USER || 'user';
 config.mysql.password = process.env.MYSQL_PASSWORD || 'password';
 config.mysql.port = process.env.MYSQL_PORT || '3306';
+config.mysql.db = process.env.MYSQL_DB || 'db';
 
 module.exports = config;
