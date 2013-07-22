43f805d68f200bf476408b650b92b30ba2ad6c89
Patrick <uhhonestly@gmail.com>
2013-07-22 18:09:05 -0400

    New EC2.

--- tests/mysql-tests.js
+++ tests/mysql-tests.js
@@ -8,7 +8,7 @@ exports.testTesting = function(test){
 
 exports.testDB = function(test){
 	  var connection = mysql.createConnection({
-  	host: 'feedback-widget.czwvwkbas6cq.us-west-2.rds.amazonaws.com;dbname=fbdb',
+  	host: 'feedback-widget.czwvwkbas6cq.us-west-2.rds.amazonaws.com',
   	user: 'priceline',
   	password: 'priceline',
   	port: '3306'
@@ -16,11 +16,14 @@ exports.testDB = function(test){
 
   connection.connect(function(err){
   	console.log(err);
- 		connection.query('CREATE TABLE feedback(ID int, fbText varchar(255))');
- 		connection.query('INSERT INTO feedback VALUES(1, test)');
- 		connection.query('SELECT * FROM feedback', function(err, rows){
- 			test.ok(rows, "rows exist");
-			test.done();
+  	connection.query('USE fbdb');
+ 		connection.query("INSERT INTO feedback VALUES('2', 'blah')", function(err, result){
+ 			if (err) throw err;
+ 			connection.query('SELECT * FROM feedback', function(err, rows){
+ 				if (err) throw err;
+ 				test.ok(rows, "rows exist");
+				test.done();
+ 			});
  		});
 	});
 }
