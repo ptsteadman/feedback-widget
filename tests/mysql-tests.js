d0051012f25019e29e21c792b35452e0bcb12d7b
Patrick <uhhonestly@gmail.com>
2013-07-22 14:16:41 -0400

    Updated tests.

--- tests/mysql-tests.js
+++ tests/mysql-tests.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var config = require('../config')
 
 exports.testTesting = function(test){
 	test.ok(true, "this should work");
@@ -17,4 +18,23 @@ exports.testConnection = function(test){
 	test.ok(connection, "connection is not undefined");
 	test.done();
 
+}
+
+exports.testDB = function(test){
+	  var connection = mysql.createConnection({
+  	host: config.mysql.host,
+  	user: config.mysql.user,
+  	password: config.mysql.password,
+  	port: config.mysql.port,
+  	database: 'feedbackdb'
+  });
+
+  connection.connect(function(err){
+ 		connection.query('CREATE TABLE feedback(ID int, fbText varchar(255))');
+ 		connection.query('INSERT INTO feedback VALUES(1, test)');
+ 		connection.query('SELECT * FROM feedback', function(err, rows){
+ 			test.ok(rows, "rows exist");
+			test.done();
+ 		});
+	});
 }
